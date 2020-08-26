55500a6c894315c91c37b401f485303400229456
rocamocha <rocamocha@cyootlabs.net>
2020-08-26 02:32:31 -1000

    0.0.1b

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $(".landing-text").hide();
-  };
+    $("#landing-text").hide();
+  }
 });
 
 $(".landing-content").mouseenter(function() {
   $(".landing-text").fadein(2000);
-}
+});
