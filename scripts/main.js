9da19e8b652b13b59099d880e2338b0a2c5e0ed0
rocamocha <rocamocha@cyootlabs.net>
2020-08-27 10:59:16 -1000

    0.0.3

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
-var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
-console.log(isTouch);
-
 $(document).ready(function() {
-  //$(".landing-text").hide();
+  $(".landing-content").click(function () {
+    //move the landing-content
+    $(".landing-content").css("animation", "landing-content-glow 2s infinite, landing-content-enter 3s forwards");
+    $(".landing-content").css("box-shadow", "0px 0px 0px 6px #FFF");
 
-  $(".landing-content").mouseenter(function() {
-    $(".landing-text").css("transition", "color 1700ms");
-  });
-  $(".landing-content").mouseleave(function() {
-    $(".landing-text").css("transition", "color 300ms");
+    //move the nav-stripe
+    $(".nav-stripe").css("animation", "nav-stripe-glow 2s infinite, nav-stripe-enter 3s forwards");
+
+    //move the landing-text
+    $(".landing-content:hover .landing-text").css("transition-delay", "initial");
+    $("#lt-left").css("top", "-112px");
+    $("#lt-left").css("opacity", "0");
+    $("#lt-right").css("top", "calc(var(--nav-stripe-enter-margin) + 1vw)");
+    $("#lt-right").css("color", "white");
+
+    //reveal the content wrapper
+    $(".content-wrapper").css("animation", "content-wrapper-reveal 3s forwards");
   });
 });
