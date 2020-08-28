1dc5c428ac9326f3ff04c5b3d647f26017cceaa9
rocamocha <rocamocha@cyootlabs.net>
2020-08-28 02:17:13 -1000

    0.0.4
    -doors functioning
    -footer added
    -visual changes
    -debug text visible

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
+
+
 $(document).ready(function() {
+  //DISABLE TOUCH FUNCTIONALITY
+  //this is to prevent pinch zooming, to keep the visual frame undisturbed
+  document.addEventListener("touchstart", function(e){
+    e.preventDefault();
+  },{passive: false});
+
+  //RE-ENABLE TOUCH FUNCTIONALITY BY ELEMENT ID
+  document.getElementById("landing-content").addEventListener("touchstart", function(e){e.stopPropagation()}, false);
+
   $(".landing-content").click(function () {
     //move the landing-content
     $(".landing-content").css("animation", "landing-content-glow 2s infinite, landing-content-enter 3s forwards");
     $(".landing-content").css("box-shadow", "0px 0px 0px 6px #FFF");
-
-    //move the nav-stripe
     $(".nav-stripe").css("animation", "nav-stripe-glow 2s infinite, nav-stripe-enter 3s forwards");
 
     //move the landing-text
@@ -16,5 +25,6 @@ $(document).ready(function() {
 
     //reveal the content wrapper
     $(".content-wrapper").css("animation", "content-wrapper-reveal 3s forwards");
+    $(".content-door").css("animation", "content-door-open 3s forwards");
   });
 });
